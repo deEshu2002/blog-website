5fa612c752cb6a6873fe6e882d606fae9a9de3a9
Pooja Mehra <poojamehra@Poojas-MacBook-Air.local>
2023-03-11 19:17:46 +0530

    added sort methods buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Navbar from "@/Components/Navbar";
 import { PageDescription } from "@/Components/PageDescription";
+import SortMethods from "@/Components/SortMethods";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function Home() {
       <main className={styles.main}>
         <Navbar />
         <PageDescription />
+        <SortMethods />
       </main>
     </>
   );
