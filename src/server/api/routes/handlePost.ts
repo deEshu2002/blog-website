c8af41fbb92e9a5e3319ea8eab012dd5d81851b8
Deepanshu <deepanshumehra2001@gmail.com>
2023-03-13 20:49:26 +0530

    altered schema and created trpc routes from backend

--- src/server/api/routes/handlePost.ts
+++ src/server/api/routes/handlePost.ts
@@ -1,15 +1,29 @@
 import { z } from "zod";
-
 import { createTRPCRouter, procedure} from "../trpc";
+import { prisma } from "@/server/db";
 
 export const handlePostRouter = createTRPCRouter({
     hello: procedure.input(z.object({text: z.string()})).query(({input}) => {
+
+        const newBlog = prisma.post.create({
+            data:{
+                author:"Tester",
+                authorId:1,
+                title:"Testers Test",
+                content:"hello hello this is the test paragraph that should be long enough to take all the space given to it so that i can test well this",
+                id:1,
+            }
+        })
+
         return {
-            greeting:  `Hello ${input.text}`,
+            inserteddata:  newBlog,
         };
     }),
 
     getAll:procedure.query(({ ctx }) => {
         // return ctx.prisma.example.findMany();
+        const allData = prisma.post.findMany();
+        
+        return allData;
     })
 })
